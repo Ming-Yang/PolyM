29a40bc86a3ae99f0d5ed538248b53596bb77d40
Ming_LES <379614985@qq.com>
2018-12-03 18:08:42 +0800

    comment 1

--- Msg.cpp
+++ Msg.cpp
@@ -8,12 +8,14 @@ namespace {
 
 MsgUID generateUniqueId()
 {
+    // 生成UID的标识符，从0开始计数，线程安全
     static std::atomic<MsgUID> i(0);
     return ++i;
 }
 
 }
 
+// 根据类型构造一个message
 Msg::Msg(int msgId)
   : msgId_(msgId), uniqueId_(generateUniqueId())
 {
@@ -21,6 +23,7 @@ Msg::Msg(int msgId)
 
 std::unique_ptr<Msg> Msg::move()
 {
+    // 返回一个MSG类型的指针，使用了移动构造函数，新建一个临时的副本，move用来减少拷贝构造的事件损失
     return std::unique_ptr<Msg>(new Msg(std::move(*this)));
 }
 
--- Queue.cpp
+++ Queue.cpp
@@ -31,13 +31,17 @@ public:
     {
         std::unique_lock<std::mutex> lock(queueMutex_);
 
+        // 如果没有规定时间，则一直等待直到队列不空
+        // 如果有规定时间，超时则获得一个超时的MSG
         if (timeoutMillis <= 0)
+        // lambda表达式，wait变量为真的时候唤醒，函数返回queue不空的时候为真
             queueCond_.wait(lock, [this]{return !queue_.empty();});
         else
         {
             // wait_for returns false if the return is due to timeout
             auto timeoutOccured = !queueCond_.wait_for(
                 lock,
+                // C++11 提供的chrono时间类，需要传入这样参数
                 std::chrono::milliseconds(timeoutMillis),
                 [this]{return !queue_.empty();});
 
@@ -45,6 +49,7 @@ public:
                 queue_.emplace(new Msg(MSG_TIMEOUT));
         }
 
+        // 依然是提供一个MSG副本的指针
         auto msg = queue_.front()->move();
         queue_.pop();
         return msg;
@@ -52,8 +57,10 @@ public:
 
     std::unique_ptr<Msg> request(Msg&& msg)
     {
+        // 新建临时队列
         // Construct an ad hoc Queue to handle response Msg
         std::unique_lock<std::mutex> lock(responseMapMutex_);
+        // emplace可以减少拷贝产生的开销
         auto it = responseMap_.emplace(
             std::make_pair(msg.getUniqueId(), std::unique_ptr<Queue>(new Queue))).first;
         lock.unlock();
